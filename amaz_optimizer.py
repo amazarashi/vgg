24c998f2e66c5b0bd7018c333b44a63678553b78
suguru <u.sgr.22114w@gmail.com>
2017-04-05 15:57:41 +0900

    changed optimizers parameters

--- amaz_optimizer.py
+++ amaz_optimizer.py
@@ -19,7 +19,7 @@ class Optimizers(object):
 
 class OptimizerVGG(Optimizers):
 
-    def __init__(self,model=None,lr=0.1,momentum=0.9,epoch=300,schedule=(50,68),weight_decay=5.0e-4):
+    def __init__(self,model=None,lr=0.01,momentum=0.9,epoch=300,schedule=(50,68),weight_decay=5.0e-4):
         super(OptimizerVGG,self).__init__(model,epoch)
         self.lr = lr
         self.optimizer = optimizers.MomentumSGD(self.lr,momentum)
@@ -29,7 +29,11 @@ class OptimizerVGG(Optimizers):
         self.schedule = schedule
 
     def update_parameter(self,current_epoch):
-        if current_epoch in self.schedule:
-            new_lr = self.lr * 0.1
-            self.optimizer.lr = new_lr
-            print("optimizer was changed to {0}..".format(new_lr))
+        # if current_epoch in self.schedule:
+        #     new_lr = self.lr * 0.1
+        #     self.optimizer.lr = new_lr
+        #     print("optimizer was changed to {0}..".format(new_lr))
+        if current_epoch % 2 == 0:
+            lr = self.lr * 0.94
+            print('lr is changed: {} -> {}'.format(self.optimizer.lr, lr))
+            self.optimizer.lr = lr
